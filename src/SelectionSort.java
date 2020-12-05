0d81b7534df94a0c4d94e4781f52535668b5333e
Dalia <nasrd@umich.edu>
2020-12-04 23:01:25 -0500

    positive and negative

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,27 +1,26 @@
-
 public class SelectionSort {
-private int temp;
-
-
-public SelectionSort()
-{
 	
+	 
+	/** Creates a new instance of SelectionSort */
+	   public SelectionSort() {
+	    }
+	    
+	  
+	    public int[] basicSelectionSort(int[] x) {
+	       for (int i = 0; i < x.length - 1; ++i) {
+	        	int min = i;
+	            for (int j= i+1; j < x.length; ++j) {
+	                if (x[j] < x[min]) {
+	                    min = j;
+	                }
+	            }
+	                    
+	                    if(min != i) {
+	                    int temp = x[min];
+	                    x[min] = x[i];
+	                    x[i] = temp;
+	                    }
+	                }
+	        return x;
+	    }
 }
-	public int[] basicSelectionSort(int[] x)
-	{
-		for(int i = 0; i <x.length; ++i)
-		{
-			for(int j = i + 1; j <x.length; ++j)
-			{
-				if(x[i] > x[j])
-				{
-					temp = x[j];
-					x[i] = x[j];
-					temp = x[j];
-				}
-			}
-		}
-		return x;
-	}
-}
-
