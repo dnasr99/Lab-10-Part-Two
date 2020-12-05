1ee6394324fe812f4f27c5af93ecdb65ad73a321
Dalia <nasrd@umich.edu>
2020-12-04 20:40:57 -0500

    Adding Files

---
+++ src/SelectionSort.java
@@ -0,0 +1,27 @@
+
+public class SelectionSort {
+private int temp;
+
+
+public SelectionSort()
+{
+	
+}
+	public int[] basicSelectionSort(int[] x)
+	{
+		for(int i = 0; i <x.length; ++i)
+		{
+			for(int j = i + 1; j <x.length; ++j)
+			{
+				if(x[i] > x[j])
+				{
+					temp = x[j];
+					x[i] = x[j];
+					temp = x[j];
+				}
+			}
+		}
+		return x;
+	}
+}
+
